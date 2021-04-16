86643429eedb1ae15fa24e44270873069d51903c
Jefferson Clebson <jeffersonclebson@gmail.com>
2021-04-15 22:12:42 -0300

    Add Global Style

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)", "../src/**/*.stories.mdx"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-viewport",
     "@storybook/addon-docs",
     "@storybook/addon-knobs",
+  ],
+};
---
+++ src/styles/Breakpoints.test.js
@@ -0,0 +1,10 @@
+import { breakAt, BreakpointSizes } from "./Breakpoints";
+
+test.each([
+  [BreakpointSizes.sm],
+  [BreakpointSizes.md],
+  [BreakpointSizes.lg],
+  [BreakpointSizes.xl],
+])("breakAt at width %ipx", (size) => {
+  expect(breakAt(size)).toEqual(`@media (min-width ${size}px)`);
+});
