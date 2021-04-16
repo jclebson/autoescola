3c76b8640a80f42c210145b3c73aa722f2747646
Jefferson Clebson <jeffersonclebson@gmail.com>
2021-04-15 22:19:21 -0300

    Add Welcome.stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toAutoEscola = () => (
+  <div>
+    <h1>Sistema Auto Escola</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, iusto
+      eveniet, laborum aliquam odit itaque aperiam illum impedit dolores nemo
+      quisquam similique earum nobis asperiores quibusdam! Vitae placeat minus
+      hic.
+    </p>
+  </div>
+);
